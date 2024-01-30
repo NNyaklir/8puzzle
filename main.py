190876217e789cd184c830aaba0b33912a6cc867
Devin <60894239+NNyaklir@users.noreply.github.com>
2024-01-29 19:06:24 -0700

    added notes

---
+++ main.py
@@ -0,0 +1,7 @@
+#student ID here
+
+#should use a 2d array for the initial and final state.
+#use hash set for storing unique boards?
+boardInitial= {}
+
+baordFinal= { 'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':0}
