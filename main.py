fe153c3fae20577f3b7b30335fb7276880f0323c
Devin <60894239+NNyaklir@users.noreply.github.com>
2024-01-31 09:07:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 
 #should use a 2d array for the initial and final state.
 #use hash set for storing unique boards?
-boardInitial= {}
+rows,cols= (2,2)
 
-baordFinal= { 'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':0}
+boardInitial= [[0]*cols]*rows
+
+boradFinal =
+
